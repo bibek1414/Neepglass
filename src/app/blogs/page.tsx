4e298327caa9c0d1cef2858a97c4a11d9c1b3798
bibek1414 <bbhattarai770@gmail.com>
2026-01-19 15:39:54 +0545

    dsad

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import BlogListingContent from "@/components/news/BlogListingContent";
 import { Metadata } from "next";
 
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function BlogListingPage() {
-    return <BlogListingContent />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <BlogListingContent />
+        </Suspense>
+    );
 }
